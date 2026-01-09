39edaed70106d93bd06e886569c6f8f2e2c18540
notesbyishika <ishikaparmar79@gmail.com>
2026-01-09 23:00:56 +0530

    Final clean-up before republish

--- practical.js
+++ practical.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const practicalBtns = document.querySelectorAll(".practical-btn");
 const pdfLists = document.querySelectorAll(".pdf-list");
 
@@ -17,24 +16,4 @@ practicalBtns.forEach(btn => {
             targetDiv.style.display = "block";
         }
     });
-=======
-const practicalBtns = document.querySelectorAll(".practical-btn");
-const pdfLists = document.querySelectorAll(".pdf-list");
-
-practicalBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        const targetId = btn.getAttribute("data-sub");
-
-        // hide all practical lists
-        pdfLists.forEach(list => {
-            list.style.display = "none";
-        });
-
-        // show selected practical
-        const targetDiv = document.getElementById(targetId);
-        if (targetDiv) {
-            targetDiv.style.display = "block";
-        }
-    });
->>>>>>> 16222117a36810f35c3e8e2c94dc5c38df6561ca
 });
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Show PDFs when subject clicked
 const subjectBtns = document.querySelectorAll(".subject-btn");
 const pdfLists = document.querySelectorAll(".pdf-list");
@@ -31,37 +30,3 @@ function searchAssignments() {
     });
 }
 
-=======
-// Show PDFs when subject clicked
-const subjectBtns = document.querySelectorAll(".subject-btn");
-const pdfLists = document.querySelectorAll(".pdf-list");
-
-subjectBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        const pdfId = btn.getAttribute("data-pdfs");
-
-        // Hide all pdf lists first
-        pdfLists.forEach(pl => pl.style.display = "none");
-
-        // Show selected pdf list
-        const currentPDFs = document.getElementById(pdfId);
-        currentPDFs.style.display = "block";
-    });
-});
-
-
-function searchAssignments() {
-    const input = document.getElementById("searchInput").value.toLowerCase();
-    const pdfLists = document.querySelectorAll(".pdf-list ul li");
-
-    pdfLists.forEach(item => {
-        const text = item.innerText.toLowerCase();
-        if(text.includes(input)) {
-            item.style.display = "list-item";
-        } else {
-            item.style.display = "none";
-        }
-    });
-}
-
->>>>>>> 16222117a36810f35c3e8e2c94dc5c38df6561ca
